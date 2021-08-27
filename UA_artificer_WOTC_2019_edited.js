e2f0ee0930c7406c9f62fbb799d5ca7b84a15149
LordFoxington <89659357+LordFoxington@users.noreply.github.com>
2021-08-27 13:07:19 -0500

    Update UA_artificer_WOTC_2019_edited.js

--- UA_artificer_WOTC_2019_edited.js
+++ UA_artificer_WOTC_2019_edited.js
@@ -368,7 +368,88 @@ ClassSubList["artificer-alchemist"] = {
         }
     }
 };
+// Forge Adept //
 
+ClassSubList["artificer-Forge Adept"] = {
+    regExpSearch: /artillerist/i,
+    subname: "Forge Adept",
+    features: {
+        "tools proficiency": {
+            name: "Tools Proficiency",
+            source: ["HB"],
+            minlevel: 3,
+            description: desc([
+                "When you adopt this specialization at 3rd level, you gain proficiency with smith’s tools.",
+                "If you already have this proficiency, you gain proficiency with one other type of artisan’s 				  tools of your choice.",
+              
+            ]),
+            toolProfs: [["Smith's Tools", "Str"]]
+        },
+        "forge adept spells": {
+            name: "Forge adept Spells",
+            source: ["HB"],
+            minlevel: 3,
+            description: desc([
+                "I always have certain spells prepared after reaching particular levels.",
+                "These spells count as artificer spells and don't count against the number of artificer spells I can prepare."
+            ]),
+            spellcastingExtra: ["Armor of Agathys", "shield of faith", "spiritual weapon", "warding bond", "beacon of hope", "remove curse", "death ward", "fire shield", "banishing smite", "wall of force"]
+        },
+        "battle ready": {
+            name: "Battle Ready",
+            source: ["HB"],
+            minlevel: 3,
+            description: desc([
+                "Beginning at 3rd level, you’re skilled in fighting side-by-side with your companions on the field of battle. You gain proficiency with martial weapons. Additionally, when you attack with a magic weapon, you can use your Intelligence modifier, instead of Strength or Dexterity, for the attack and damage rolls.",
+         
+         "Legendary Weapon": {
+            name: "Legendary Weapon",
+            source: ["HB"],
+            minlevel: 3,
+            description: desc([
+            	"At 3rd level, you learn to imbue a weapon with a fraction of your spirit. When you finish a long rest, you can touch a magic or nonmagical simple or martial melee weapon, which becomes your Legendary Weapon.
+
+Once imbued, your Legendary Weapon is a magic weapon that grants a +1 bonus to attack and damage rolls, unless the base weapon you imbued already grants a higher bonus. This bonus increases when you reach certain levels in this class, increasing to +2 at 8th level and +3 at 13th level. If the weapon you imbued has the thrown property, it returns to your hand immediately after it is used to make a ranged attack.
+
+Your Legendary Weapon’s benefits remain until the weapon is destroyed or until you use this ability to imbue a new item with this power. Other creatures can’t benefit from your Legendary Weapon's powers, and to them, it functions as the unimbued weapon did.",
+            ]) 
+        },
+        "extra attack": {
+            name: "Extra Attack",
+            source: ["HB"],
+            minlevel: 5,
+            description: desc([
+                "Starting at 5th level, you can attack twice, rather than once, whenever you take the Attack action on your turn.",
+            ])
+        },
+        "Runes of War": {
+            name: "Runes of War",
+            source: ["HB"],
+            minlevel: 9,
+            description: desc([
+            	"At 9th level, you gain the ability to enhance the weapons of your squad, targeting the damage to the weakness of your enemies. As an action, you can activate an aura that extends from you in a 30-foot radius. The aura moves with you, centered on you, and lasts for 1 minute or until your concentration ends (as if you were casting a spell).
+
+When you activate this aura, choose one of the following damage types: acid, cold, fire, lightning, or thunder. Each creature of your choice in the aura deals an extra 1d4 damage of the chosen type when it hits with a weapon attack.
+
+You can use this feature a number of times equal to your Intelligence modifier (minimum of once), and you regain all expended uses after a long rest.",
+            ])
+        },    
+        "Perfect Weapon": {
+            name: "Perfect Weapon",
+            source: ["HB"],
+            minlevel: 15,
+            description: desc([
+                "At 15th level, you learn how to imbue your Legendary Weapon with additional power, and how to draw on it in time of need. The first time you attack with your Legendary Weapon on your turn, you can transfer some or all of its bonus to your armor class, instead of using the bonus on any attacks that turn. For every 1 you reduce your attack and damage rolls by, you gain +1 to your AC. As long as you continue holding your Legendary Weapon, the AC bonus remains in effect until the start of your next turn.
+
+Additionally, when you finish a long rest, you can select one of the following benefits. Until you finish your next long rest, you gain the selected benefit whenever you’re holding your Legendary Weapon.
+
+You have resistance to psychic damage, and you can’t be charmed or frightened.
+
+You use your Legendary Weapon to channel the elements. When you select this ability, select one of the following damage types: acid, cold, fire, lightning, or thunder. When you hit with your Legendary Weapon, the target takes an extra 1d6 damage of the chosen type."
+            ])
+        }      
+    }
+};
 // Artillerist //
 
 ClassSubList["artificer-artillerist"] = {
